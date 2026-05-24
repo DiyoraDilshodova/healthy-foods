3b209cb2025573f3d5727f23f8a86f46d88894c0
Diyora Dilshodova <dilshodovadiyora2010@gmail.com>
2026-05-24 15:29:34 +0500

    menu-finished

---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,28 @@
+import "./menu.css";  
+
+const Menu = () => {
+  return (
+    <section className="menu">
+      <div className="container">
+        <span className="menu-text">menu</span>
+        <h2 className="menu-title">Food Full of treaty Love</h2>
+        <p className="menu-description">
+          There are many things are needed to start the Fast Food Business. You
+          need not only Just Food Stalls with Persons but also specialized
+          equipment, Skills to manage Customers,{" "}
+        </p>
+        <div className="products">
+            <div className="product"></div>
+            <div className="product"></div>
+            <div className="product"></div>
+            <div className="product"></div>
+            <div className="product"></div>
+            <div className="product"></div>
+        </div>
+        <button className="menu-btn">Learn More</button>
+      </div>
+    </section>
+  );
+};
+
+export default Menu;
